85297cce9a93cc584b7db0c3313d6f24096734f0
Viktor Kirichok <viktor.kirichok@gmail.com>
2020-10-06 11:42:13 +0300

    Updates

---
+++ precache-manifest.a24d0f86f48bea01609ee5a15a14ca22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c248abec585e704f3ba78d7b5bca9f9b",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "e46975aeafda4337bbda",
+    "url": "/goit-react-hw-02-feedback/static/css/main.7e66f903.chunk.css"
+  },
+  {
+    "revision": "6ed1063afa45ba41d9f0",
+    "url": "/goit-react-hw-02-feedback/static/js/2.79dbc3f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-feedback/static/js/2.79dbc3f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46975aeafda4337bbda",
+    "url": "/goit-react-hw-02-feedback/static/js/main.ae65a74f.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
